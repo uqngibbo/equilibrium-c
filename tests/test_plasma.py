155e09c8f0190807263a1877c83999809aa3ce53
Nick Gibbons <n.gibbons@uq.edu.au>
2020-10-16 17:47:00 +1000

    Added rhou and ps test to test_plasma

--- tests/test_plasma.py
+++ tests/test_plasma.py
@@ -25,54 +25,54 @@ class TestCEQ(unittest.TestCase):
         assert_array_almost_equal(Xs1, Xst, decimal=4)
         return
 
-#    def test_rhou(self):
-#        spnames = ['N2', 'O2', 'N', 'O', 'NO', 'NO+', 'e-']
-#        T = 9000.0
-#        p = 0.1*101.35e3
-#        Xs0 = array([1.2680e-3, 0.0, 7.7128e-1, 2.2490e-1, 2.8465e-5, 1.3e-3, 1.0e-3])
-#        Xs0/=(Xs0.sum())
-#        Xst = array([1.2680e-3, 0.0, 7.7128e-1, 2.2490e-1, 2.8465e-5, 1.2625e-3, 1.2625e-3])
-#        ceq = pyeq.EqCalculator(spnames)
-#
-#        Mt = sum(Xst*ceq.M)
-#        Rt = Ru/Mt
-#        rhot = p/Rt/T
-#        nt = 1/Mt
-#        cst = Xst*rhot/Mt
-#        nst = Xst/Mt # also cs/rhot 
-#        nt2 = nst.sum()
-#
-#        ut = ceq.get_u(Xst, T)
-#
-#        Xs1, Teq = ceq.rhou(rhot, ut, Xs0, 0)
-#        assert_array_almost_equal(Xs1, Xst, decimal=8)
-#        self.assertAlmostEqual(Teq, T, 4)
-#        return
-#
-#    def test_ps(self):
-#        spnames = ['N2', 'O2', 'N', 'O', 'NO', 'NO+', 'e-']
-#        T = 9000.0
-#        p = 0.1*101.35e3
-#        Xs0 = array([1.2680e-3, 0.0, 7.7128e-1, 2.2490e-1, 2.8465e-5, 1.3e-3, 1.0e-3])
-#        Xs0/=(Xs0.sum())
-#        Xst = array([1.2680e-3, 0.0, 7.7128e-1, 2.2490e-1, 2.8465e-5, 1.2625e-3, 1.2625e-3])
-#        ceq = pyeq.EqCalculator(spnames)
-#
-#        Mt = sum(Xst*ceq.M)
-#        Rt = Ru/Mt
-#        rhot = p/Rt/T
-#        nt = 1.0/Mt
-#        cst = Xst*rhot/Mt
-#        nst = Xst/Mt # also cs/rhot 
-#        nt2 = nst.sum()
-#
-#        s0 = ceq.get_s0(Xst, T)
-#        st = ceq.get_s(Xst, T, p)
-#        pt = p
-#
-#        Xs1, Teq = ceq.ps(pt, st, Xs0, 0)
-#        assert_array_almost_equal(Xs1, Xst, decimal=8)
-#        self.assertAlmostEqual(Teq, T, 4)
+    def test_rhou(self):
+        spnames = ['N2', 'O2', 'N', 'O', 'NO', 'NO+', 'e-']
+        T = 9000.0
+        p = 0.1*101.35e3
+        Xs0 = array([1.2680e-3, 0.0, 7.7128e-1, 2.2490e-1, 2.8465e-5, 1.3e-3, 1.0e-3])
+        Xs0/=(Xs0.sum())
+        Xst = array([1.2680e-3, 0.0, 7.7128e-1, 2.2490e-1, 2.8465e-5, 1.2625e-3, 1.2625e-3])
+        ceq = pyeq.EqCalculator(spnames)
+
+        Mt = sum(Xst*ceq.M)
+        Rt = Ru/Mt
+        rhot = p/Rt/T
+        nt = 1/Mt
+        cst = Xst*rhot/Mt
+        nst = Xst/Mt # also cs/rhot 
+        nt2 = nst.sum()
+
+        ut = ceq.get_u(Xst, T)
+
+        Xs1, Teq = ceq.rhou(rhot, ut, Xs0, 0)
+        assert_array_almost_equal(Xs1, Xst, decimal=4)
+        self.assertAlmostEqual(Teq, T, 0)
+        return
+
+    def test_ps(self):
+        spnames = ['N2', 'O2', 'N', 'O', 'NO', 'NO+', 'e-']
+        T = 9000.0
+        p = 0.1*101.35e3
+        Xs0 = array([1.2680e-3, 0.0, 7.7128e-1, 2.2490e-1, 2.8465e-5, 1.3e-3, 1.0e-3])
+        Xs0/=(Xs0.sum())
+        Xst = array([1.2680e-3, 0.0, 7.7128e-1, 2.2490e-1, 2.8465e-5, 1.2625e-3, 1.2625e-3])
+        ceq = pyeq.EqCalculator(spnames)
+
+        Mt = sum(Xst*ceq.M)
+        Rt = Ru/Mt
+        rhot = p/Rt/T
+        nt = 1.0/Mt
+        cst = Xst*rhot/Mt
+        nst = Xst/Mt # also cs/rhot 
+        nt2 = nst.sum()
+
+        s0 = ceq.get_s0(Xst, T)
+        st = ceq.get_s(Xst, T, p)
+        pt = p
+
+        Xs1, Teq = ceq.ps(pt, st, Xs0, 0)
+        assert_array_almost_equal(Xs1, Xst, decimal=4)
+        self.assertAlmostEqual(Teq, T, 0)
 
 if __name__=='__main__':
     unittest.main()
