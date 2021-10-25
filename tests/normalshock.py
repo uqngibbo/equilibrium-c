607bac4d322cd288a6993354df8cb0410a83ed8c
Nick N. Gibbons <n.gibbons@uq.edu.au>
2021-10-25 11:04:13 +1000

    Changed normalshock.py test to solve a single equation in the postshock velocity v

--- tests/normalshock.py
+++ tests/normalshock.py
@@ -1,23 +1,19 @@
 """
 Application script: Compute a normal shock with ionization
 
+v2: With pressure error method.
+
 @author: Nick Gibbons
 """
 
-from numpy import zeros, array
+from numpy import zeros, array, linspace
 from collections import namedtuple
-from scipy.optimize import root, newton 
-from copy import copy
+from scipy.optimize import newton 
 import pyeq
 
 Ru = 8.314
 class GasState(object):
-    def __init__(self, p, T, v, X0, ceq):
-        p = max(100.0, p) # Optimizer sometimes likes to take WILD guesses on these
-        T = max(20.0, T)  # which can go negative. This was enough to keep them converging.
-        self.p=p; self.T=T; self.v=v; self.X0=X0; self.ceq=ceq
-        
-        X = ceq.pt(p, T, X0)
+    def __init__(self, rho, T, p, u, v, X, ceq):
         h = ceq.get_h(X, T)
         s = ceq.get_s(X, T, p)
         cp= ceq.get_cp(X, T)
@@ -26,20 +22,44 @@ class GasState(object):
 
         Mmix = (Ms*X).sum()
         R = Ru/Mmix
-        rho = p/R/T
         cv = cp-R
         k = cp/cv
         Y = X*Ms/Mmix
 
-        self.X = X; self.h = h; self.s = s; self.cp=cp; self.Ms= Ms; self.spnames=spnames;
-        self.Mmix = Mmix; self.R = R; self.rho = rho; self.cv=cv; self.k = k; self.Y = Y
+        self.T = T; self.p = p; self.u = u; self.v = v; self.X = X; self.ceq = ceq;
+        self.h = h; self.s = s; self.cp=cp; self.Ms= Ms; self.spnames=spnames;
+        self.Mmix = Mmix; self.R = R; self.rho = rho; self.cv=cv; self.k = k; self.Y = Y; self.X0=X;
 
         self.a = self.soundspeed()
         self.M = self.Mach_number()
-        return
+
+    @classmethod
+    def from_pTv(cls, p, T, v, X0, ceq):
+        p = max(1.0, p)  # Optimizer sometimes likes to take WILD guesses on these
+        T = max(1.0, T)  # which can go negative. This was enough to keep them converging.
+        
+        X = ceq.pt(p, T, X0)
+        u = ceq.get_u(X, T)
+
+        Mmix = (ceq.M*X).sum()
+        R = Ru/Mmix
+        rho = p/R/T
+
+        return cls(rho, T, p, u, v, X, ceq)
+
+    @classmethod
+    def from_rhouv(cls, rho, u, v, X0, ceq):
+        X, T = preshock.ceq.rhou(rho, u, X0, 0)
+        Mmix = (ceq.M*X).sum()
+        R = Ru/Mmix
+        p = rho*R*T
+        return cls(rho, T, p, u, v, X, ceq)
 
     def new_from_pTv(self, p, T, v):
-        return GasState(p, T, v, X0=self.X0, ceq=self.ceq)
+        return GasState.from_pTv(p, T, v, X0=self.X0, ceq=self.ceq)
+
+    def new_from_rhouv(self, rho, u, v):
+        return GasState.from_rhouv(rho, u, v, X0=self.X0, ceq=self.ceq)
 
     def soundspeed(self):
         return (self.k*self.R*self.T)**0.5
@@ -56,20 +76,42 @@ class GasState(object):
         ]
         return '\n'.join(s)
 
+def fluxes(gs):
+    rho = gs.rho; v = gs.v; p = gs.p;
 
-def Rankine_Huginot_Error(s1, s2):
-    mass = s1.rho*s1.v - s2.rho*s2.v
-    mom = (s1.rho*s1.v**2 + s1.p)  - (s2.rho*s2.v**2 + s2.p)
-    enth = (s1.h + s1.v**2/2.0)  - (s2.h + s2.v**2/2.0)
-    error = array([mass**2, mom**2, enth**2])
-    return error
+    Fmass = rho*v
+    Fspecies = [Fmass*Y for Y in gs.Y]
+    Fmom = rho*v**2 + p
+    Fenergy = v*(rho*gs.u + 0.5*rho*v**2 + p)
+    return Fmass, Fspecies, Fmom, Fenergy
 
-def F(primitives, preshock):
-    """ Compute the Rankine Huginot jump error from the preshock state in the incident shock frame """
-    p,T,v = primitives
-    postshock = preshock.new_from_pTv(p,T,v)
-    error = Rankine_Huginot_Error(preshock, postshock) 
-    return error 
+def rhopu_from_v(v, preshock):
+    Fmass, Fspecies, Fmom, Fenergy = fluxes(preshock)
+    Y0 = array([Fsps/Fmass for Fsps in Fspecies])
+    X0 = preshock.ceq.YtoX(Y0)
+
+    rho = Fmass/v
+    p = Fmom - rho*v**2
+    u = (Fenergy - 0.5*rho*v**3 - p*v)/(Fmass)
+    return rho, p, u
+
+def pressure_error_from_v(v, preshock):
+    v = max(10.0, v) # We have to cap this at a positive number, to prevent negative rho
+    rho, p, u = rhopu_from_v(v, preshock)
+    postshock = preshock.new_from_rhouv(rho, u, v)
+
+    # Here is where things start to get weird...
+    
+    # We've computed the entire gas state using the fluxes and a guessed velocity
+    # v. But that makes no sense because *any* v that we chose would have given us
+    # an answer??? The key is that there is one equation we haven't used yet, the
+    # ideal gas equation of state p = rho*R*T. Inside that new_from_rhouv call
+    # above, we computed a pressure using the ideal gas equation.  That pressure is
+    # actually inconsistent with the pressure calculated above from the momentum flux.
+    # And so: We can adjust the guess velocity v until the two match up and that
+    # will give the correct postshock state.
+
+    return (p - postshock.p)/p
 
 def guess(s1):
     """ Guess initial postshock conditions using ideal gas behaviour """
@@ -86,24 +128,27 @@ if __name__=='__main__':
     spnames = ['N2', 'N2+', 'NO', 'NO+', 'O2', 'O2+', 'N', 'N+', 'O', 'O+', 'e-']
     X0 = array([0.77, 0.0, 0.0, 0.0, 0.23, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
     X0/=(X0.sum())
-    p1=40000; T1=300; vi=1777.73
+    p1=40000; T1=300; vi=3000.0
 
     ceq = pyeq.EqCalculator(spnames)
 
-    preshock = GasState(p=p1, T=T1, v=vi, X0=X0, ceq=ceq)
-
-    # Compute a normal shock by solving for function F=0.0
+    preshock = GasState.from_pTv(p=p1, T=T1, v=vi, X0=X0, ceq=ceq)
     start = guess(preshock)
-    sinfo = root(F, start, args=(preshock,))
-    p,T,v = sinfo.x
-
-    postshock = GasState(p=p, T=T, v=v, X0=X0, ceq=ceq)
-
     print("preshock:\n", preshock, '\n')
+    print("guess:\n p={} T={} v={}\n".format(*start))
+
+    # Compute a normal shock by solving for function f=0.0
+    f = lambda v : pressure_error_from_v(v, preshock)
+    vpostshock = newton(f, start[2]) # guess velocity is start[2]
+
+    # With the correct velocity found, compute the postshock state
+    rho, p, u = rhopu_from_v(vpostshock, preshock)
+    postshock = preshock.new_from_rhouv(rho, u, vpostshock)
+    ionisation_fraction = postshock.X[spnames.index('e-')]*2
+
     print("postshock:\n", postshock, '\n')
 
-    print("Mass fractions: ")
-    for spname, mass_fraction in zip(postshock.spnames, postshock.Y):
-        print(spname, mass_fraction)
+    print("Ionization fraction: {:g} %".format(ionisation_fraction*100))
+
 
 
