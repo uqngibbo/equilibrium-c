3ee2c9acc66a7761ba1697ebc07bbd4f50833441
Nick N. Gibbons <n.gibbons@uq.edu.au>
2023-08-01 15:18:39 +1000

    Fixed to the windows compile script, including naming the library file libceq.dll

--- source/compile.ps1
+++ source/compile.ps1
@@ -1,4 +1,5 @@
-gcc -I. -fPIC -Wall -std=c99 -O3 -c .\thermo.c gcc -I. -fPIC -Wall -std=c99 -O3 -c .\linalg.c
+gcc -I. -fPIC -Wall -std=c99 -O3 -c .\thermo.c
+gcc -I. -fPIC -Wall -std=c99 -O3 -c .\linalg.c
 gcc -I. -fPIC -Wall -std=c99 -O3 -c .\common.c
 gcc -I. -fPIC -Wall -std=c99 -O3 -c .\pt.c
 gcc -I. -fPIC -Wall -std=c99 -O3 -c .\rhot.c
@@ -6,4 +7,4 @@ gcc -I. -fPIC -Wall -std=c99 -O3 -c .\rhou.c
 gcc -I. -fPIC -Wall -std=c99 -O3 -c .\ps.c
 gcc -I. -fPIC -Wall -std=c99 -O3 -c .\ceq.c
 
-gcc -I. -fPIC -Wall -std=c99 -O3 -shared .\thermo.o .\linalg.o .\common.o .\pt.o .\rhou.c .\rhot.o .\ps.o .\ceq.o -lm -o .\libceq.so
+gcc -I. -fPIC -Wall -std=c99 -O3 -shared .\thermo.o .\linalg.o .\common.o .\pt.o .\rhou.c .\rhot.o .\ps.o .\ceq.o -lm -o .\libceq.dll
